b098115c0d72b03c2c4185156e9b100e852a8faa
Vlad Serhiienko <vladserhiienko92@gmail.com>
2017-02-02 13:23:45 +0200

    adding matchbook project

---
+++ assets/js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    $(".main-slider").slick({
+        "arrows": true,
+        "dots": false,
+        "autoplay": true,
+        "draggable": false
+    });
+
+    $(".slick-prev.slick-arrow").text("Prev");
+
+});
